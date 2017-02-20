93cff055c9f194f6e5267bbf1d34ca59dba59bf5
wiltonvgc <wiltondesc@gmail.com>
2017-02-20 14:09:44 -0300

    lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -23,16 +23,16 @@ class Topologia(Topo):
         super(Topologia, self ).__init__()
 	
 	#Criacao dos hosts
-        pc11 = self.addNode('pc11')#host pc1.1
-	pc12 = self.addNode('pc12')#host pc1.2
-	pc21 = self.addNode('pc21')#host pc2.1
-	pc22 = self.addNode('pc22')#host pc2.2
-	pc31 = self.addNode('pc31')#host pc3.1
-	pc32 = self.addNode('pc32')#host pc3.2
-	pc81 = self.addNode('pc81')#host pc8.1
-	pc82 = self.addNode('pc82')#host pc8.2
-	pc91 = self.addNode('pc91')#host pc9.1
-	pc92 = self.addNode('pc92')#host pc9.2
+        pc11 = self.addNode('pc11',ip='192.168.1.1')#host pc1.1
+	pc12 = self.addNode('pc12',ip='192.168.1.2')#host pc1.2
+	pc21 = self.addNode('pc21',ip='192.168.2.1')#host pc2.1
+	pc22 = self.addNode('pc22',ip='192.168.2.2')#host pc2.2
+	pc31 = self.addNode('pc31',ip='192.168.3.1')#host pc3.1
+	pc32 = self.addNode('pc32',ip='192.168.3.2')#host pc3.2
+	pc81 = self.addNode('pc81',ip='192.168.8.1')#host pc8.1
+	pc82 = self.addNode('pc82',ip='192.168.8.2')#host pc8.2
+	pc91 = self.addNode('pc91',ip='192.168.9.1')#host pc9.1
+	pc92 = self.addNode('pc92',ip='192.168.9.2')#host pc9.2
 
 	#Criacao dos switchs
 	sL1 = self.addSwitch('sL1')#switch LAN1
@@ -80,11 +80,15 @@ def getGateway(host):
 	return ip
 
 def main():
-    os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*")
+    os.system("rm -f /tmp/r*.log /tmp/r*.pid logs/*")
     os.system("mn -c >/dev/null 2>&1")	
+    os.system("killall -9 zebra > /dev/null 2>&1")
+    os.system('pgrep -f webserver.py | xargs kill -9')
 
     net = Mininet(topo=Topologia())
     net.start()
+
+	
 
     #Seta roteador rA e rB
     for router in net.switches:
@@ -92,15 +96,19 @@ def main():
         	router.cmd("sysctl -w net.ipv4.ip_forward=1")
         	router.waitOutput()
 
-    #Configura interfaces dos hosts
-    for host in net.hosts:
-	print("ifconfig %s-eth0 %s" % (host.name, getIP(host.name)))
-	host.cmd("ifconfig %s-eth0 %s" % (host.name, getIP(host.name)))
-	host.cmd("route add default gw %s" % getGateway(host.name))
-
+    #Configura interface do roteador rA e rB com zebra
+    for router in net.switches:
+        if(router.name=='r1' or router.name=='r2'):
+       		router.cmd("/usr/lib/quagga/zebra -f conf/zebra-%s.conf -d -i /tmp/zebra-%s.pid > logs/%s-zebra-stdout 2>&1" % (router.name, router.name, router.name))
+        	router.waitOutput()
+		#router.cmd("/usr/lib/quagga/bgpd -f conf/bgpd-%s.conf -d -i /tmp/bgp-%s.pid > logs/%s-bgpd-stdout 2>&1" % (router.name, router.name, router.name), shell=True)
+        	#router.waitOutput()
+		
+  
     CLI(net)
-    net.stop()
-    #os.system("killall -9 zebra bgpd")
+    net.stop() 
+    os.system("killall -9 zebra")
+   
 
 if __name__ == "__main__":
     main()
